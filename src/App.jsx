74f78b77d26b3749a044aa9db82055e9fd1ed53f
FonHartmann <110547106+FonHartmann@users.noreply.github.com>
2022-08-04 15:57:52 +0300

    Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
-import { Layout } from "./components/Layout";
+import { Layout } from "./components/Layohut"h
 import { Home } from "./pages/Homepage";
 import { Login } from "./pages/LogIn";
 
